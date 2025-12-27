9a18cdd9becbcf9bf9b7d710ddff56c531809651
GabrielForestiero <gabyforestiero@gmail.com>
2025-12-27 15:08:24 -0300

    integracion prisma

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-import express from 'express';  
+require('dotenv').config();
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: 'Energy Citrus',
-      price: 19.99,
-      description: 'Bebida energética sabor citrus.',
-      imageURL: 'https://hp.widen.net/content/dak8r5vaqq/png/dak8r5vaqq.png?w=800&h=600&dpi=72&color=ffffff00'
-    },
-    {
-      id: 2,
-      name: 'Energy Berry',
-      price: 21.99,
-      description: 'Bebida energética sabor frutos rojos.',
-      imageURL: 'https://hp.widen.net/content/dak8r5vaqq/png/dak8r5vaqq.png?w=800&h=600&dpi=72&color=ffffff00'
-    }
-  ]);
+app.get('/products', async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
 });
 
 app.listen(port, () => {
-    console.log('server running on port', port);
+  console.log('server running on port', port);
 });
